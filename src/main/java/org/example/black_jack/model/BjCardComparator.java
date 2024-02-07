760563c6b5eead267143269098978e0fa4b9668f
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2024-02-08 01:42:29 +0200

    feat: do not sort cards if one of them is hidden

--- src/main/java/org/example/black_jack/model/BjCardComparator.java
+++ src/main/java/org/example/black_jack/model/BjCardComparator.java
@@ -7,6 +7,9 @@ import java.util.Comparator;
 public class BjCardComparator implements Comparator<Card> {
     @Override
     public int compare(Card first, Card second) {
+        if(!first.isOpen() || !second.isOpen()) {   //не сортируем, если карта закрыта
+            return 0;
+        }
         return first.getRank().ordinal() - second.getRank().ordinal() ;
     }
 }
